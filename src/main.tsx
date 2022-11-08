1cadf3739c3d230974fda54fe04deedd40612356
dssh27 <dssh27@gmail.com>
2022-11-08 14:23:38 -0300

    ADD: React-queryy devtool en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router';
-import { router } from './router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { router } from "./router";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 //! Remover enable css source maps
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={ router } />
-  </React.StrictMode>
-)
+const client = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <QueryClientProvider client={client}>
+            <RouterProvider router={router} />
+            <ReactQueryDevtools />
+        </QueryClientProvider>
+    </React.StrictMode>
+);
