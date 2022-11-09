b1871827a212c430147986a90485f18badceec3e
dssh27 <dssh27@gmail.com>
2022-11-09 20:11:25 -0300

    Label picker editado

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,32 @@
+import { useLabels } from "../hooks/useLabels";
+import { LoadingIcon } from "../../shared/components/LoadingIcon";
+import { FC } from "react";
 
-export const LabelPicker = () => {
-  return (
-    <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
-    </div>
-  )
+interface Props {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
 }
+
+export const LabelPicker: FC<Props> = ({ selectedLabels, onChange }) => {
+  const labelsQuery = useLabels();
+
+  if (labelsQuery.isFetching) return <LoadingIcon />;
+
+  return (
+    <>
+      {labelsQuery.data?.map((label) => (
+        <span
+          key={label.id}
+          className={`badge rounded-pill m-1 label-picker ${selectedLabels.includes(label.name) ? "label-active" : null}`}
+          style={{
+            border: `1px solid #${label.color}`,
+            color: `#${label.color}`,
+          }}
+          onClick={() => onChange(label.name)}
+        >
+          {label.name}
+        </span>
+      ))}
+    </>
+  );
+};
