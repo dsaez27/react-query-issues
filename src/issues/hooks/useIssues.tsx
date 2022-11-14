50c6af0e7478c8f66bfc0a1cb2733d1edf20800a
dssh27 <dssh27@gmail.com>
2022-11-13 21:08:20 -0300

    Agregados filtros

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -1,14 +1,33 @@
-import { Issue } from "../interfaces";
+import { Issue, State } from "../interfaces";
 import { githubApi } from "../api/githubAPi";
 import { useQuery } from "@tanstack/react-query";
+import { sleep } from "../../helpers/sleep";
 
-const getIssues = async (): Promise<Issue[]> => {
-    const { data } = await githubApi.get<Issue[]>("/issues");
-    console.log(data);
+interface Props {
+    state?: State;
+    labels: string[];
+}
+
+const getIssues = async (labels: string[] = [], state?: State): Promise<Issue[]> => {
+    await sleep(2);
+
+    const params = new URLSearchParams();
+
+    if (state) params.append("state", state);
+
+    if (labels.length > 0) {
+        const labelString = labels.join(",");
+        params.append("labels", labelString);
+    }
+
+    params.append("page", "1");
+    params.append("per_page", "5");
+
+    const { data } = await githubApi.get<Issue[]>("/issues", { params });
     return data;
 };
 
-export const useIssues = () => {
-    const issuesQuery = useQuery(["issues"], getIssues);
+export const useIssues = ({ state, labels }: Props) => {
+    const issuesQuery = useQuery(["issues", { state, labels }], (data) => getIssues(labels, state));
     return { issuesQuery };
 };
