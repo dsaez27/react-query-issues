ec25dadc9d65216ec5ccf45a3cd83204dceca363
dssh27 <dssh27@gmail.com>
2022-11-13 21:05:20 -0300

    modificado tiempo y agregado labels

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -4,6 +4,7 @@ import { FiInfo, FiMessageSquare, FiCheckCircle } from "react-icons/fi";
 import { Issue, State } from "../interfaces";
 import { useQueryClient } from "@tanstack/react-query";
 import { getIssueInfo, getIssueComments } from "../hooks/useIssue";
+import { timeSince } from '../../helpers/time-since';
 
 interface Props {
     issue: Issue;
@@ -41,9 +42,20 @@ export const IssueItem: FC<Props> = ({ issue }) => {
                 <div className="d-flex flex-column flex-fill px-2">
                     <span>{issue.title}</span>
                     <span className="issue-subinfo">
-                        #{issue.number} opened 2 days ago by {issue.user.login}
-                        <span className="fw-bold">{issue.user.login}</span>
+                        #{issue.number} opened { timeSince(issue.created_at) } ago by
+                        <span className="fw-bold">{" " + issue.user.login}</span>
                     </span>
+                    <div>
+                        {issue.labels.map((label) => (
+                            <span
+                                key={label.id}
+                                className="badge rounded-pill m-1"
+                                style={{ backgroundColor: `#${label.color}`, color: "black" }}
+                            >
+                                {label.name}
+                            </span>
+                        ))}
+                    </div>
                 </div>
 
                 <div className="d-flex align-items-center">
