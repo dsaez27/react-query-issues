9cafe456cf6b76dd105a4183f8020ef7e4a713a7
dssh27 <dssh27@gmail.com>
2022-11-13 21:03:53 -0300

    Cambio api key

--- src/issues/api/githubAPi.ts
+++ src/issues/api/githubAPi.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const githubApi = axios.create({
     baseURL: "https://api.github.com/repos/facebook/react",
     headers: {
-        Authorization: 'Bearer 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'
+        Authorization: 'Bearer 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'
     },
 });
