a7e3d7ebf8d12f6dc589c142e2d276812b301ab2
dssh27 <dssh27@gmail.com>
2022-11-13 21:11:37 -0300

    Agregado estado issueList y filtros en issuesQuery

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -3,24 +3,30 @@ import { IssueList } from "../components/IssueList";
 import { LabelPicker } from "../components/LabelPicker";
 import { useIssues } from "../hooks";
 import { LoadingIcon } from "../../shared/components/LoadingIcon";
+import { State } from "../interfaces";
 
 export const ListView = () => {
     const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
-    const { issuesQuery } = useIssues();
+    const [state, setState] = useState<State>();
+    const { issuesQuery } = useIssues({ state, labels: selectedLabels });
 
     const onLabelChanged = (labelName: string) => {
         selectedLabels.includes(labelName)
             ? setSelectedLabels(selectedLabels.filter((label) => label !== labelName))
             : setSelectedLabels([...selectedLabels, labelName]);
     };
-
+ 
     return (
         <div className="row mt-5">
             <div className="col-8">
                 {issuesQuery.isLoading ? (
                     <LoadingIcon />
                 ) : (
-                    <IssueList issues={issuesQuery.data || []} />
+                    <IssueList
+                        issues={issuesQuery.data || []}
+                        state={state}
+                        onStateChanged={(newState) => setState(newState)}
+                    />
                 )}
             </div>
 
